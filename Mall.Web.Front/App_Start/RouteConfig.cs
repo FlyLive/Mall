fa5f5c9548f06910dce096c7dac1171aa71f0592
CLOUDY2016 <1585213801@qq.com>
2017-01-29 15:57:05 +0800

    主页完善

--- Mall.Web.Front/App_Start/RouteConfig.cs
+++ Mall.Web.Front/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Mall.Web.Front
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
